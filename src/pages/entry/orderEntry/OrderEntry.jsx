f51c2d9e595886ba39a39988506c544bcb9d6ac5
Shiyu Wang <shiyu3169@gmail.com>
2021-12-05 12:14:55 -0800

    test disable order button if there are no scoops ordered

--- src/pages/entry/orderEntry/OrderEntry.jsx
+++ src/pages/entry/orderEntry/OrderEntry.jsx
@@ -4,13 +4,20 @@ import Button from 'react-bootstrap/Button'
 
 export default function OrderEntry({ setOrderPhase }) {
   const [orderDetails] = useOrderDetails()
+
+  const disableOrderButton = orderDetails.totals.scoops === '$0.00'
   return (
     <>
       <h1>Design Your Sundae!</h1>
       <Options optionType='scoops' />
       <Options optionType='toppings' />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
-      <Button onClick={() => setOrderPhase('review')}>Order Sundae!</Button>
+      <Button
+        disabled={disableOrderButton}
+        onClick={() => setOrderPhase('review')}
+      >
+        Order Sundae!
+      </Button>
     </>
   )
 }
